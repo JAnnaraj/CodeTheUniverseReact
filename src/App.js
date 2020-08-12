b254182fb9847a1393aa05e2ffbf0b3d82d712d5
Jeffrey Annaraj <47981217+JAnnaraj@users.noreply.github.com>
2020-08-11 22:25:10 -0500

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>This is a sample React App</h1>
+      <p>This is working.</p>
+    </div>
+  );
+}
+
+export default App;
